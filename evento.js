2a92d2e5f70ec49fc7e059efa228fd04d13fd938
cerdate <cerdateinformatica@gmail.com>
2021-01-05 21:52:26 -0300

    projeto netflix

---
+++ evento.js
@@ -0,0 +1,30 @@
+let btn = document.querySelector('.navbar-toggler');
+let navbar =document.querySelector('.navbar');
+
+btn.addEventListener('click' , e => {
+
+    navbar.classList.toggle('sidebar-open');
+
+     if (navbar.classList.contains('sidebar-open')) {
+
+        const backdrop = document.createElement('div');
+        backdrop.classList.add('backdrop');
+
+        backdrop.addEventListener('click', e => {
+
+            navbar.classList.remove('sidebar-open');
+            document.querySelector('.backdrop').remove();
+
+        });
+
+        document.querySelector('body').append(backdrop);
+
+        
+
+     } else {
+
+            document.querySelector('.backdrop').remove();
+
+     }
+
+});
